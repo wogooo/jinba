19be5640737baaa86d1e8eb706203edd2efd14c6
daqi <daqidaqi@qq.com>
2015-05-22 16:34:09 +0800

    output

--- fis-conf.js
+++ fis-conf.js
@@ -1,4 +1,4 @@
-fis.config.set('roadmap.domain', 'http://static.idaiyan.cn/special/h5_20150515_haochanpin');
+fis.config.set('roadmap.domain', 'http://tjvvv.com/demo/jinba');
 
 //后缀名的less的文件使用fis-parser-less编译
 //modules.parser.less表示设置后缀名为less的文件的parser，第二个less表示使用fis-parser-less进行编译
@@ -7,8 +7,14 @@ fis.config.set('modules.parser.less', 'less');
 fis.config.set('roadmap.ext.less', 'css');
 
 fis.config.set('roadmap.path', [
+
     {
         reg: /\/_[^\/]*?$/i,
         release: false
+    },
+    {
+        reg : /readme.md|fis-conf.js/,
+        //编译的时候不要产出了
+        release : false
     }
 ]);
